6ad06ddcaa481e7861e9bc98146849e3d73035e1
alizulfiqarbuksh <ali.zulfiqarbuksh@gmail.com>
2025-11-23 11:06:58 +0300

    Added paddle movement

--- js/app.js
+++ js/app.js
@@ -31,6 +31,13 @@ const p2Paddle = {
   speed: 6,
 };
 
+let p1UpPressed = false;
+let p1DownPressed = false;
+let p2UpPressed = false;
+let p2DownPressed = false;
+
+let maxDown = canvasEl.height - p1Paddle.height;
+
 // Functions ---------------------------------------------------------
 function createBall () {
   ctx.beginPath();
@@ -47,7 +54,35 @@ function clearCanvas () {
       ctx.clearRect(0, 0, 600, 400);
 };
 
+function paddleMovement () {
+  if (p1UpPressed === true) {
+    p1Paddle.positionY -= p1Paddle.speed;
+    if (p1Paddle.positionY < 0) {
+      p1Paddle.positionY = 0;
+    }
+  }
+  if (p1DownPressed === true) {
+    p1Paddle.positionY += p1Paddle.speed;
+    if (p1Paddle.positionY > maxDown) {
+      p1Paddle.positionY = maxDown;
+    }
+  }
+  if (p2UpPressed === true) {
+    p2Paddle.positionY -= p2Paddle.speed;
+    if (p2Paddle.positionY < 0) {
+      p2Paddle.positionY = 0;
+    }
+  }
+  if (p2DownPressed === true) {
+    p2Paddle.positionY += p2Paddle.speed;
+    if (p2Paddle.positionY > maxDown) {
+      p2Paddle.positionY = maxDown;
+    }
+  }
+};
+
 function displayElements () {
+  paddleMovement();
   clearCanvas();
   createBall();
   createPaddle(p1Paddle);
@@ -57,3 +92,37 @@ function displayElements () {
 
 displayElements();
 
+// Events ---------------------------------------------------------
+document.addEventListener('keydown', (event) => {
+  if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+    event.preventDefault();
+  }
+
+  if (event.key === "w") {
+    p1UpPressed = true;
+  }
+  else if (event.key === "s") {
+    p1DownPressed = true;
+  }
+  else if (event.key === "ArrowUp") {
+    p2UpPressed = true;
+  }
+  else if (event.key === "ArrowDown") {
+    p2DownPressed = true;
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  if (event.key === "w") {
+    p1UpPressed = false;
+  }
+  else if (event.key === "s") {
+    p1DownPressed = false;
+  }
+  else if (event.key === "ArrowUp") {
+    p2UpPressed = false;
+  }
+  else if (event.key === "ArrowDown") {
+    p2DownPressed = false;
+  }
+});
